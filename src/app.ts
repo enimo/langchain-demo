00dde0b8ba94193f04e13bf0681f99d05521573f
enimo <enimong@gmail.com>
2023-03-07 01:38:48 +0800

    fix call chain init and add webpage summary

--- src/app.ts
+++ src/app.ts
@@ -64,8 +64,6 @@ console.log({ res });
 //Import the OpenAPI Large Language Model (you can import other models here eg. Cohere)
 import { OpenAI, OpenAIChat } from "langchain/llms";
 
-//Import the Vector DB QA chain
-import { VectorDBQAChain } from "langchain/chains";
 
 //Import the Hierarchical Navigable Small World Graphs vector store (you'll learn
 //how it is used later in the code)
@@ -82,19 +80,28 @@ import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 //Import file stystem node module
 import * as fs from "fs";
 
+import { Document } from "langchain/document"
+
 
 import { TextLoader } from "langchain/document_loaders";
 import { PDFLoader } from "langchain/document_loaders";
 import { CheerioWebBaseLoader } from "langchain/document_loaders";
 
 
+import { loadSummarizationChain } from "langchain/chains";
+import { VectorDBQAChain } from "langchain/chains";
+import { ChatVectorDBQAChain } from "langchain/chains";
+
+import pdfParse from "pdf-parse"
+
+
 export const run = async () => {
+
   //Instantiante the OpenAI LLM that will be used to answer the question
   // const model = new OpenAI({ cache: true });
   const model = new OpenAIChat({
     openAIApiKey: process.env.OPENAI_API_KEY,
     modelName: "gpt-3.5-turbo",
-    // prefixMessages: history,
     temperature: 0.7,
     verbose: true,
     cache: true,
@@ -116,40 +123,117 @@ export const run = async () => {
 
 
 
-  //Create documents from the split text as required by subsequent calls
-  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+  // 1 读取文件，建立Document对象{pageContent:xxx, metadata:xxx}
+  // 1.1 使用fs读取pdf文件, failed
+  // const pdfBuffer = fs.readFileSync("miniprogram_ads_doc.pdf");
+  // const { text } = await pdfParse(pdfBuffer)
 
-  // const text = fs.readFileSync("state_of_the_union.txt", "utf8");
+  // const rawDocs = new Document({ pageContent: text })
+
+  // const textSplitter = new RecursiveCharacterTextSplitter({
+  //   chunkSize: 1000,
+  //   chunkOverlap: 200,
+  // })
+  // const docs = await textSplitter.splitDocuments([rawDocs])
+
+
+  // 1.2 使用fs读取txt文件 OK
+  //Create documents from the split text as required by subsequent calls
+   // const text = fs.readFileSync("state_of_the_union.txt", "utf8");
+  // const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
   // const docs = await textSplitter.createDocuments([text]);
 
-  const loader = new TextLoader("state_of_the_union.txt");
 
-  // const loader = new PDFLoader("src/document_loaders/example_data/example.pdf");
-  const docs = await loader.load();
+  // 1.3 使用loader读取PDF 和 txt ok
+  // const loader = new TextLoader("state_of_the_union.txt");
+  // const loader = new PDFLoader("miniprogram_ads_doc.pdf");
+  const loader = new PDFLoader("resume.pdf");
+  const rawDocs = await loader.load();
+  // console.log({ docs });
+
+  // 1.4 读取网页内容：https://kodango.com/useful-documents-about-shell
+  // const loader = new CheerioWebBaseLoader(
+// 	  "https://kodango.com/useful-documents-about-shell"
+// 	);  
+  // const rawDocs = await loader.load();
   // console.log({ docs });
 
 
+  // 1.9 完成读取后 必须进行切块
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+  })
+  const docs = await textSplitter.splitDocuments( rawDocs )
+
+
+
+  // 2 开始建立索引 hnswlib是一个内存矢量数据库，可以换成可持久化的矢量数据库，如部署pgvector, 或PineconeStore云服务
   //Create the vector store from OpenAIEmbeddings
   //OpenAIEmbeddings is used to create a vector representation of a text in the documents.
   //Converting the docs to the vector format and storing it in the vectorStore enables LangChain.js
   //to perform similarity searches on the "await chain.call"
   const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
 
-  //Create the LangChain.js chain consting of the LLM and the vector store
-  const chain = VectorDBQAChain.fromLLM(model, vectorStore);
 
+
+
+  // 3 开始提问
+  //Create the LangChain.js chain consting of the LLM and the vector store
   //Ask the question that will use content from the file to find the answer
   //The way this all comes together is that when the following call is made, the "query" (question) is used to
   //search the vector store to find chunks of text that is similar to the text in the "query" (question). Those
   //chunks of text are then sent to the LLM to find the answer to the "query" (question). This is done because,
   //as explained earlier, the LLMs have a limit in size of the text that can be sent to them
-  const res = await chain.call({
-    input_documents: docs,
-    // query: "What did the president say about the Cancer Moonshot? answer me in zh_CN",
-    query: "What is this about? answer me in zh_CN",
-  });
-  console.log({ res });
+  
+
+  	/*
+	// 3.1 Call the summarization chain, ok 
+	const chain = loadSummarizationChain(model);
+	const res = await chain.call({
+	  input_documents: docs,
+	  // query: '请用中文回答',
+	  // question: '请用中文回答'
+	});
+	console.log(res);
+	*/
+
+	
+	/*
+  	 //3.2 Chat  one single qusetion
+  	const chain = VectorDBQAChain.fromLLM(model, vectorStore);
+
+	const res = await chain.call({
+	    input_documents: docs,
+	    // query: "What did the president say about the Cancer Moonshot? answer me in zh_CN",
+	    query: "What is this about? Answer me in zh_CN",
+	});
+	console.log({ res });
+	*/
+
+
+	
+  	//Chat  history
+	const chain = ChatVectorDBQAChain.fromLLM(model, vectorStore);
+	const question = "What is this about? answer me in zh_CN";
+	const res = await chain.call({ question: question, chat_history: [] });
+	console.log(res);
+
+	// Ask it a follow up question 
+	const chatHistory = question + res["text"];
+	const followUpRes = await chain.call({
+	  question: "作者是谁，用中文回答",
+	  chat_history: chatHistory,
+	});
+	console.log(followUpRes);
+	
+
+
+
 };
+
 run();
+
+
 
 
